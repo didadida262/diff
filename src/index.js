5cbf4efac665af2f6dccec783149d141b6d26ae2
wangziqin <719713496@qq.com>
2021-09-26 22:51:17 +0800

    diffffff

--- src/index.js
+++ src/index.js
@@ -13,18 +13,18 @@ import {
   
 
   const myPatch = (oldNode, newNode) => {
+    console.log('oldNode:', oldNode)
+    console.log('newNode:', newNode)
     //   改造dom为虚拟节点，主义elm属性
       if (!oldNode.sel) {
           oldNode = myVnode(oldNode.tagName.toLowerCase(), {}, [], undefined, oldNode)
       } 
-      console.log('oldNode:', oldNode)
-      console.log('newNode:', newNode)
     //   判断是否为同一个dom
       if (oldNode.key === newNode.key && oldNode.sel === newNode.sel) {
         patchVnode(oldNode, newNode)
       } else {
+        console.log('不是同一节点')
         //   不是同一个节点,暴力删除
-        console.log('不同节点')
         const newNodeElm = createElement(newNode)
         // console.log('newNodeElm:', newNodeElm)
         oldNode.elm.parentNode.insertBefore(newNodeElm, oldNode.elm)
@@ -32,12 +32,17 @@ import {
       }
   }
 
-  // const newNode1 = myH('div', {}, 'oldnode')
-  const newNode1 = myH('div', {}, [ myH('li', {}, 'a'), myH('li', {}, 'b'), myH('li', {}, 'c')])
+  // const newNode1 = myH('ul', {}, 'oldnode')
+  const newNode1 = myH('ul', {}, 
+    [ myH('li', { key: 't' }, 'a'), 
+      myH('li', { key: 'b' }, 'b'), 
+      myH('li', { key: 'c' }, 'c')])
 
   // const newNode2 = myH('div', {}, 'newnode')
-  const newNode2 = myH('div', {}, [ 
-    myH('li', {}, 'a'), myH('li', {}, 'b'), myH('li', {}, 'c'), myH('li', {}, 'd')])
+  const newNode2 = myH('ul', {}, 
+    [ myH('li', { key: 'a' }, 'dasdasd'), 
+      myH('li', { key: 'b' }, 'b'), 
+      myH('li', { key: 'c' }, 'c')])
 
   myPatch(container, newNode1)
 
--- src/sn.js
+++ src/sn.js
@@ -6,7 +6,6 @@ import {
     eventListenersModule,
     h,
   } from "snabbdom";
-  
     //   将vnode --> 真实dom，返回
     const createElement = (vnode) => {
         const domNode = document.createElement(vnode.sel)
@@ -56,6 +55,30 @@ import {
       }
   }
 
+  const checkSameNode = (oldNode, newNode) => {
+      return oldNode.sel === newNode.sel && oldNode.key === newNode.key
+  }
+
+  const updateChildren = (oldElm, oldCh, newCh) => {
+      let oldStartIdx = 0
+      let oldEndIdx = oldCh.length - 1
+      let newStartIdx = 0
+      let newEndIdx = newCh.length - 1
+      let oldStartVnode = oldCh[0]
+      let oldEndVnode = oldCh[oldEndIdx]
+      let newStartVnode = newCh[0]
+      let newEndVnode = newCh[newEndIdx]
+
+
+      while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+          if (checkSameNode(oldStartVnode, newStartVnode)) {
+              patchVnode(oldStartVnode, newStartVnode)
+              oldStartVnode = oldCh[++oldStartIdx]
+              newStartVnode = newCh[++newStartIdx]
+          }
+      }
+  }
+
   const patchVnode = (oldNode, newNode) => {
         if (oldNode === newNode) return
         // 新节点存在text属性，即：没有children。
@@ -71,12 +94,11 @@ import {
             if (Array.isArray(oldNode.children) && oldNode.children.length > 0) {
               // 有,diff的精华
               // for (let i =0; i < )
-
-
-              
+              updateChildren(oldNode.elm, oldNode.children, newNode.children)
             } else {
                 // o没有
                 console.log('老东西没有儿子')
+                newNode.elm = oldNode.elm
                 oldNode.elm.innerText = ''
                 for (const item of newNode.children) {
                   oldNode.elm.appendChild(createElement(item))                  
