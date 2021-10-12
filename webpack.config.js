9f785400f2592d2fad6df58982a55e00cfabf11e
wangziqin <719713496@qq.com>
2021-10-12 23:05:16 +0800

    import-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: ['style-loader', 
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1
+                }
+              }, 
+              'postcss-loader']
       },
       {
         test: /\.less$/,
